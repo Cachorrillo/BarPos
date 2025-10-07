bf40235a14acc3028120d880e1074fa59295ea54
Vi <vicnightwolf@gmail.com>
2025-10-06 18:25:48 -0600

    Inventario funcional

---
+++ BarPos/Models/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarPos.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Presentacion> Presentaciones { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=VÍCTOR\\SQLEXPRESS;Database=BarPOS;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC07BFF0842C");
+        });
+
+        modelBuilder.Entity<Presentacion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Presenta__3214EC07A2645688");
+
+            entity.HasOne(d => d.Producto).WithMany(p => p.Presentaciones)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Presentaciones_Productos");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC074A802B3B");
+
+            entity.HasOne(d => d.Categoria).WithMany(p => p.Productos)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Productos_Categorias");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BarPos/Models/Presentacion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BarPos.Models;
+
+public partial class Presentacion
+{
+    [Key]
+    public long Id { get; set; }
+
+    public long ProductoId { get; set; }
+
+    [StringLength(50)]
+    public string Nombre { get; set; } = null!;
+
+    [BindRequired]
+    public decimal? PrecioVenta { get; set; }
+
+    public int? CantidadEquivalente { get; set; }
+
+    [ForeignKey("ProductoId")]
+    [InverseProperty("Presentaciones")]
+    public virtual Producto Producto { get; set; } = null!;
+}
