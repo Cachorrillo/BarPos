101fe2336ec0d4e73d92fab95413ea7fb4150b93
JeffersonSolano <solanodaniel266@gmail.com>
2025-10-31 18:32:24 -0600

    Resubido para ver si le funciona a Victor

--- BarPos/obj/Debug/net8.0/BarPos.AssemblyInfo.cs
+++ BarPos/obj/Debug/net8.0/BarPos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarPos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d62e19a50e2831a12c752400d9127eb0e7721bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6e335011b8f794229c64609f8f31d91ee26c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarPos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarPos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
